2ee17b0afebb93cb530ab6502288e8f4f3453ef8
NovaEclipseZ <21frostfire@gmail.com>
2024-04-22 10:58:14 -0500

    4/22/24/11:00

--- Tic_Tac_Toe/Tic_Tac_Toe/View/TicTacToeTUI.java
+++ Tic_Tac_Toe/Tic_Tac_Toe/View/TicTacToeTUI.java
@@ -2,6 +2,7 @@ package View;
 import java.util.Scanner;
 
 import Model.AI;
+import Model.Gameboard;
 import Model.Player;
 
 public class TicTacToeTUI
@@ -9,7 +10,13 @@ public class TicTacToeTUI
     public static void run()
     {
         Scanner input = new Scanner(System.in);
+        Gameboard game = new Gameboard();
         Player user = new Player("test name");
         AI computer = new AI();
+        boolean won = false;
+        while(!won)
+        {
+
+        }
     }
 }
